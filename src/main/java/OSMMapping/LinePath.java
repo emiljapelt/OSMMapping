ba5de57fae98d2ffcf902292edc9917381914f53
emiljapelt <ejapelt@hotmail.com>
2020-02-20 08:46:53 +0100

    Change to line size check

--- src/main/java/OSMMapping/LinePath.java
+++ src/main/java/OSMMapping/LinePath.java
@@ -29,12 +29,14 @@ public class LinePath implements Drawable{
 
     public void trace(GraphicsContext gc, double scale){
         gc.moveTo(coordinates[0], coordinates[1]);
+        float latestX = coordinates[0];
+        float latestY = coordinates[1];
         for (int i = 2 ; i < coordinates.length ; i += 2) {
-            //TODO review size check
-            if( 0.6 < scale * Math.sqrt(Math.pow((coordinates[i] - coordinates[i-2]),2) + Math.pow((coordinates[i+1] - coordinates[i-1]),2))) {
+            if( 0.6 < scale * Math.sqrt(Math.pow((coordinates[i] - latestX),2) + Math.pow((coordinates[i+1] - latestY),2))) {
                 gc.lineTo(coordinates[i], coordinates[i + 1]);
+                latestX = coordinates[i];
+                latestY = coordinates[i + 1];
             }
-
         }
     }
 }
